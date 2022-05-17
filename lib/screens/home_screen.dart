a0c857d8986d3e8b673f650a869bfc34a2d24755
Milan Yadav <ymilan593@gmail.com>
2022-05-17 16:23:48 +0545

    created routes and screens

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: 2,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return ElevatedButton(onPressed: () {}, child: Text('$index'));
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Sign Up'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/signin');
+          },
+        ),
+      ),
+    );
+  }
+}
